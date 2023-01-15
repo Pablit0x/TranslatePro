8b243d9647ce90b55604ec01eed9d36677a37dcc
noner <pawel495@interia.pl>
2023-01-15 14:32:00 +0000

    Setting up the Translation Client

---
+++ shared/src/commonMain/kotlin/com/ps/translatepro/translate/data/translate/KtorTranslateClient.kt
@@ -0,0 +1,4 @@
+package com.ps.translatepro.translate.data.translate
+
+class KtorTranslateClient {
+}
---
+++ shared/src/commonMain/kotlin/com/ps/translatepro/translate/data/translate/TranslatedDto.kt
@@ -0,0 +1,3 @@
+package com.ps.translatepro.translate.data.translate
+
+data class TranslatedDto()
