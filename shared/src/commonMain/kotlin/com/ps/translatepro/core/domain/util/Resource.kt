b53d9f55d61eee1f34828a57f377c3adeb0b5755
noner <pawel495@interia.pl>
2023-01-17 20:55:15 +0000

    Adding the translate use case and the outcome identifier

---
+++ shared/src/commonMain/kotlin/com/ps/translatepro/core/domain/util/Resource.kt
@@ -0,0 +1,6 @@
+package com.ps.translatepro.core.domain.util
+
+sealed class Resource<T>(val data: T?, val throwable: Throwable? = null){
+    class Success<T>(data: T) : Resource<T>(data =data)
+    class Error<T>(throwable: Throwable) : Resource<T>(data = null, throwable = throwable)
+}
---
+++ shared/src/commonMain/kotlin/com/ps/translatepro/translate/domain/translate/Translate.kt
@@ -0,0 +1,39 @@
+package com.ps.translatepro.translate.domain.translate
+
+import com.ps.translatepro.core.domain.language.Language
+import com.ps.translatepro.core.domain.util.Resource
+import com.ps.translatepro.translate.domain.history.HistoryDataSource
+import com.ps.translatepro.translate.domain.history.HistoryItem
+
+class Translate(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String> {
+        return try{
+            val translatedText = client.translate(
+                fromLanguage = fromLanguage,
+                fromText = fromText,
+                toLanguage = toLanguage
+            )
+
+            historyDataSource.insertHistoryItem(
+                HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = fromLanguage.langCode,
+                    toText = translatedText
+                )
+            )
+            Resource.Success(translatedText)
+        } catch (e : TranslateException){
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+}
