9c50b985ac93b15fab599eaa9e5335cd754b3a52
noner <pawel495@interia.pl>
2023-02-22 20:10:08 +0000

    Fixing the IOS workspace...

--- iosApp/iosApp/core/presentation/GradientSurface.swift
+++ iosApp/iosApp/core/presentation/GradientSurface.swift
@@ -6,36 +6,6 @@
 //  Copyright © 2023 orgName. All rights reserved.
 //
 
-//import SwiftUI
-//
-//struct GradientSurface: ViewModifier {
-//    @Environment(\.colorScheme) var colorScheme
-//
-//    func body(content: Content) -> some View {
-//        if colorScheme == .dark {
-//            let gradientStart = Color.darkSurfaceStart
-//            let gradientEnd = Color.darkSurfaceEnd
-//            content
-//                .background(
-//                    LinearGradient(
-//                        gradient: Gradient(colors: [gradientStart, gradientEnd]),
-//                        startPoint: .top,
-//                        endPoint: .bottom
-//                    )
-//                )
-//        } else {
-//            content.background(Color.surface)
-//        }
-//    }
-//}
-//
-//extension View {
-//    func gradientSurface() -> some View {
-//        modifier(GradientSurface())
-//    }
-//}
-//
-
 import SwiftUI
 
 struct GradientSurface: ViewModifier {
@@ -43,12 +13,10 @@ struct GradientSurface: ViewModifier {
     
     func body(content: Content) -> some View {
         if colorScheme == .dark {
-            let gradientStart = Color(hex: 0xFF23262E)
-            let gradientEnd = Color(hex: 0xFF212329)
             content
                 .background(
                     LinearGradient(
-                        gradient: Gradient(colors: [gradientStart, gradientEnd]),
+                        gradient: Gradient(colors: [.darkSurfaceStart, .darkSurfaceEnd]),
                         startPoint: .top,
                         endPoint: .bottom
                     )
